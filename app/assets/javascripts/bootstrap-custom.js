fa690fbe0e33e61a444f1a658516b66e654940f3
Denis Krivostchokov <densomart@gmail.com>
2014-08-15 15:01:04 +0700

    herolu assets precompile fail

--- app/assets/javascripts/bootstrap-custom.js
+++ app/assets/javascripts/bootstrap-custom.js
@@ -1,8 +1,8 @@
 //= require bootstrap/affix
 //= require bootstrap/alert
 //= require bootstrap/button
-# require bootstrap/carousel
-# require bootstrap/collapse
+//= require bootstrap/carousel
+//= require bootstrap/collapse
 //= require bootstrap/dropdown
 //= require bootstrap/tab
 //= require bootstrap/transition
