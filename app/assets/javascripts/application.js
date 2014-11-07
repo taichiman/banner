418c0ed6a52678641d7bbb319f1ec3066b200eb9
Denis Krivostchokov <densomart@gmail.com>
2014-11-07 19:06:29 +0600

    bootstrap nav-tabs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,19 @@
-//= require_tree .
 
 //= require jquery
 //= require jquery_ujs
+
+//= require_tree .
+
+function log(){
+	try{
+		console.log.apply(console,arguments);
+	}
+	catch(e){
+		try{
+			opera.postError.apply(opera,arguments);
+		}
+		catch(e){
+			alert(Array.prototype.join.call(arguments,""));
+		}
+	}
+}
